13f4c32402af7556b701c6eba95caff0ac7e5de7
Wynand Badenhorst <badenhorst@wynand.ca>
2019-02-12 21:08:36 -0600

    got it to read from a file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace csPractice
 {
@@ -6,7 +7,20 @@ namespace csPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader("ebnf.txt"))
+                {
+                // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadToEnd().Replace(";\r\n",";;").Replace("\r\n","").Replace(";;",";\r\n");
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
